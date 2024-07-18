2ed637e0d5a236cb7bb26e78281dbc574a48bb9a
Benjamin Farnham <20448279+paper-krane@users.noreply.github.com>
2024-07-18 12:04:30 -0600

    Updated assets

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/interior-designby-kimberly/dist/',
+  base: '/interior-design-by-kimberly/dist/',
   // base: '/',
   plugins: [vue()],
   resolve: {
